f10c5087a3c45f129e10ebf02fa7e76d17680238
JC-d3v <calderon.rjorge@gmail.com>
2021-06-24 21:53:50 -0400

    avance al jun-24-n

---
+++ Udy-2020/01-fundamentos/app.js
@@ -0,0 +1,43 @@
+// este es un comentario de una sola linea
+
+/*
+est ees un comentario de multiples lineas
+que usa dos signos /   y   *
+*/
+var primerNombre = 'Jorge';
+console.log(primerNombre);
+
+var year = 2021;
+var account = 1234.254;
+console.log(year);
+console.log(account);
+
+var single = true;
+console.log(single);
+
+var work;
+console.log(position);
+
+var position = null;
+console.log(position);
+
+var dayWeek, month;
+dayWeek = 'jueves';
+month = 'junio'
+
+console.log('el dia es ' + dayWeek + ' del mes de '+ month)
+
+var opera, salesToday, salesYesterday;
+salesToday=5;
+salesYesterday=10;
+opera = salesToday + salesYesterday;
+console.log(opera);
+
+opera = salesToday - salesYesterday;
+console.log(opera);
+
+opera = salesToday * salesYesterday;
+console.log(opera);
+
+opera = salesToday / salesYesterday;
+console.log(opera);
---
+++ Udy-2020/04-sentencias-y-bucles/app.js
@@ -0,0 +1,125 @@
+/*
+** ejemplode sentencias 
+** Sentencia if else
+** Sintaxis
+if (condición) sentencia1 [else sentencia2]
+** 
+*/
+
+var nombre, estadoCivil, registrado;
+nombre='jorge';
+estadoCivil='Soltero';
+registrado=false;
+
+if (registrado) { 
+   // salida verdadera
+   console.log(nombre +' es una persona registrada')
+} else{
+   //salida falso
+   console.log(nombre +' no esta registrado')
+}
+
+/****
+ * sentencias condicionales
+ * and or not
+ */
+var edad, nombre;
+edad=116;
+nombre='jorge';
+if (edad<=12) {
+   console.log(nombre + ' es un niño');
+}else if (edad<=16){
+   console.log(nombre + ' es un adolescente');
+}else  console.log(nombre + ' es un adulto');
+
+// AND
+if ((edad>12)&&(edad<18)) {
+   console.log(nombre + ' es un adolescente');
+} else  console.log(nombre + ' NO es un adolescente');
+
+
+// Operador ternario
+   edad >=18 ? console.log(nombre + ' es un mayor') : console.log(nombre + ' es menor');
+
+   // sentencia switch
+var mes;
+mes = 2
+switch(mes){
+   case 1:
+      console.log('enero');
+      break;
+   case 2:
+      console.log('febrero');
+      break;
+   case 3:
+      console.log('marzo');
+      break;
+   case 4:
+      console.log('abril');
+      break;
+   default:
+      console.log('mes no considerado');
+}
+
+// sentencia FOR
+var i;
+for (i=1;i<=10;i++){
+   console.log('for '+ i);
+} 
+
+// Sentencia While
+
+var i=1;
+while(i <=10){
+   console.log('while'+i);
+   i+=2;
+}
+
+// sentencia DO WHILE 
+var i;
+i=1;
+do {
+   console.log('while'+i);
+   i+=2;
+}
+while (i<=10);
+
+/** VAlores FALSOS Y VERDADEROS
+ * SON FALSOS SI: undefined, null, 0, ''
+ * SON VERDADEROS SI: NOT valores falsos
+ */
+
+var valor123;
+// valor123=null;
+// valor=0;
+// valor='';
+if (valor123){
+   console.log('VERDADERO')
+} else{
+   console.log('FALSO');
+}
+
+
+/** Iguales y estrictamente iguales
+ * doble signo de igualdad == iguales
+ * triple signo de igualdad === estrictamente iguales
+ */
+
+var a,b;
+a=2;
+b='2';
+if (a==b)  console.log('son Iguales') 
+else
+   console.log("no son iguales");
+
+if (a===b)  console.log('son estrictamente Iguales') 
+   else
+      console.log("no son estrictamenbte iguales");
+      
+
+// MATRIZ
+var notas, promedio;
+
+notas=[51,51,51];
+promedio=notas[1];
+
