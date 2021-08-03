fa4c908ac68c95d93a231e8afa27c320143bdaa0
JC-d3v <calderon.rjorge@gmail.com>
2021-08-03 14:33:49 -0400

    folder 4 sentencias y bucles

--- Udy-2020/04-sentencias-y-bucles/app.js
+++ Udy-2020/04-sentencias-y-bucles/app.js
@@ -7,16 +7,16 @@ if (condición) sentencia1 [else sentencia2]
 */
 
 var nombre, estadoCivil, registrado;
-nombre='jorge';
-estadoCivil='Soltero';
-registrado=false;
+nombre = 'jorge';
+estadoCivil = 'Soltero';
+registrado = false;
 
-if (registrado) { 
+if (registrado) {
    // salida verdadera
-   console.log(nombre +' es una persona registrada')
-} else{
+   console.log(nombre + ' es una persona registrada ;-)');
+} else {
    //salida falso
-   console.log(nombre +' no esta registrado')
+   console.log(nombre + ' no esta registrado .-(');
 }
 
 /****
@@ -24,27 +24,26 @@ if (registrado) {
  * and or not
  */
 var edad, nombre;
-edad=116;
-nombre='jorge';
-if (edad<=12) {
+edad = 116;
+nombre = 'jorge';
+if (edad <= 12) {
    console.log(nombre + ' es un niño');
-}else if (edad<=16){
+} else if (edad <= 16) {
    console.log(nombre + ' es un adolescente');
-}else  console.log(nombre + ' es un adulto');
+} else console.log(nombre + ' es un adulto');
 
 // AND
-if ((edad>12)&&(edad<18)) {
+if (edad > 12 && edad < 18) {
    console.log(nombre + ' es un adolescente');
-} else  console.log(nombre + ' NO es un adolescente');
-
+} else console.log(nombre + ' NO es un adolescente');
 
 // Operador ternario
-   edad >=18 ? console.log(nombre + ' es un mayor') : console.log(nombre + ' es menor');
+edad >= 18 ? console.log(nombre + ' es un mayor') : console.log(nombre + ' es menor');
 
-   // sentencia switch
+// sentencia switch
 var mes;
-mes = 2
-switch(mes){
+mes = 2;
+switch (mes) {
    case 1:
       console.log('enero');
       break;
@@ -63,26 +62,25 @@ switch(mes){
 
 // sentencia FOR
 var i;
-for (i=1;i<=10;i++){
-   console.log('for '+ i);
-} 
+for (i = 1; i <= 10; i++) {
+   console.log('for ' + i);
+}
 
 // Sentencia While
 
-var i=1;
-while(i <=10){
-   console.log('while'+i);
-   i+=2;
+var i = 1;
+while (i <= 10) {
+   console.log('while' + i);
+   i += 2;
 }
 
-// sentencia DO WHILE 
+// sentencia DO WHILE
 var i;
-i=1;
+i = 1;
 do {
-   console.log('while'+i);
-   i+=2;
-}
-while (i<=10);
+   console.log('while' + i);
+   i += 2;
+} while (i <= 10);
 
 /** VAlores FALSOS Y VERDADEROS
  * SON FALSOS SI: undefined, null, 0, ''
@@ -93,33 +91,61 @@ var valor123;
 // valor123=null;
 // valor=0;
 // valor='';
-if (valor123){
-   console.log('VERDADERO')
-} else{
+if (valor123) {
+   console.log('VERDADERO');
+} else {
    console.log('FALSO');
 }
-
 
 /** Iguales y estrictamente iguales
  * doble signo de igualdad == iguales
  * triple signo de igualdad === estrictamente iguales
  */
 
-var a,b;
-a=2;
-b='2';
-if (a==b)  console.log('son Iguales') 
-else
-   console.log("no son iguales");
+var a, b;
+a = 2;
+b = '2';
+if (a == b) console.log('son Iguales');
+else console.log('no son iguales');
 
-if (a===b)  console.log('son estrictamente Iguales') 
-   else
-      console.log("no son estrictamenbte iguales");
-      
+if (a === b) console.log('son estrictamente Iguales');
+else console.log('no son estrictamenbte iguales');
 
 // MATRIZ
 var notas, promedio;
 
-notas=[51,51,51];
-promedio=notas[1];
+notas = [51, 51, 51];
+promedio = notas[1];
 
+/*
+ *
+ * Tenemos dos alumnos Pablo y Maria
+ * cada uno de ellos tiene sus calificaciones
+ *
+ * calccular el promedio de cada alumno
+ * indicar como aprobado si supera 60pts
+ *
+ */
+
+var promedioPablo = (70 + 80 + 60) / 3;
+var promedioMaria = (7 + 80 + 60) / 3;
+
+if (promedioPablo > promedioMaria) {
+   console.log('El promedio de pablo es mayor');
+} else if (promedioPablo < promedioMaria) {
+   console.log('El promedio de maria es mayor');
+} else {
+   console.log('los promedios son iguales');
+}
+
+if (promedioPablo > 51) {
+   console.log('pablo aprobo');
+} else {
+   console.log('Pablo no aprobo');
+}
+
+if (promedioMaria > 51) {
+   console.log('maria aprobo');
+} else {
+   console.log('maria no aprobo');
+}
