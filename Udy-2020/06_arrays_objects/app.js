4110199b370fd1d496c72ee72eb6677e1539e66c
JC-d3v <calderon.rjorge@gmail.com>
2021-09-01 23:15:41 -0400

    Manipulaciones DOM

--- Udy-2020/06_arrays_objects/app.js
+++ Udy-2020/06_arrays_objects/app.js
@@ -14,13 +14,69 @@ fruits.forEach(function (elemento, indice, array) {
    console.log(elemento, indice);
 });
 console.log(fruits);
+
 fruits.push('mango');
 console.log(fruits);
+
 fruits.unshift('pera');
 console.log(fruits);
+
 fruits.pop();
 console.log(fruits);
+
 fruits.shift();
 console.log(fruits);
+
 var position = fruits.indexOf('Limon');
 console.log(position);
+
+fruits.splice(1, 2);
+console.log(fruits);
+
+//arreglos con diferentes tipos
+
+var persona = ['nombre', 'App', 33, 408888, 5.6];
+console.log(persona);
+
+// creando objetos en forma literal
+
+var perro = {
+   nombre: 'Fernando',
+   edad: 2.11,
+   color: 'dorado',
+   juguetes: ['sogas', 'pelotas', 'palos'],
+};
+console.log(perro);
+console.log(perro.color);
+console.log(perro['nombre']);
+
+perro.nombre = 'Fernando Alonzo';
+console.log(perro);
+
+// Creando obejtos con la sintaxis object
+
+var dog = new Object();
+dog.nombre = 'fernando A';
+dog.edad = '2 años';
+dog.color = 'doradito claro';
+
+console.log(dog);
+
+// objetos y metodos
+var perrito = {
+   //propiedades
+   nombre: 'Fernandito',
+   edad: 2.11,
+   color: 'dorado',
+   juguetes: ['sogas', 'pelotas', 'palos'],
+   cantidad: 3,
+   palabras: 'ns/nr',
+   //metodos
+   ladra: function () {
+      this.palabras = this.cantidad + 'guau' + this.palabras;
+   },
+};
+console.log(perrito);
+
+perrito.ladra();
+console.log(perrito);
---
+++ Udy-2020/07_manipulacion_DOM/app.js
@@ -0,0 +1,12 @@
+//EXAMINANDO EL DOM
+// console.dir(document);
+
+console.log(document.title);
+document.title = 'test01';
+console.log(document.title);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[10]);
+console.log(document.forms[0]);
+console.log(document.links);
