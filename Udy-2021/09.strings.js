8f27327126c5a31240c9f7d46d01b6a3da93c98b
JC-d3v <calderon.rjorge@gmail.com>
2021-09-12 23:04:48 -0400

    udy-2021 metodos string

--- Udy-2021/09.strings.js
+++ Udy-2021/09.strings.js
@@ -1,4 +1,5 @@
 let frase = 'que es uno que no es uno si no son dos';
+let frase2 = '  que    es   uno   ';
 
 // TIP: ANCHOR: Crea una etiqueta HTML ancla, con el NAME:test
 document.write('<p>' + 'ANCHOR: ' + frase.anchor('test'));
@@ -49,5 +50,48 @@ document.write('<p>' + 'length: ' + frase.length);
 // TIP: LINK: crea un enlace HTML A
 document.write('<p>' + 'link: ' + frase.link('test'));
 
-// TIP: Nos devuelve la coincidencia si existe o NULL sino
-document.write('<p>' + 'match: ' + frase.match('dors'));
+// TIP: MATCH Nos devuelve la coincidencia si existe o NULL sino
+document.write('<p>' + 'match: ' + frase.match('dos'));
+
+// TIP: REPEAT Repite la frase n veces
+document.write('<p>' + 'repeat: ' + frase.repeat(2));
+
+// TIP: replace: permite reemplazar en la salida un segmento de cadena por otra
+document.write('<p>' + 'replace: ' + frase.replace());
+document.write('<p>' + 'replace: ' + frase.replace('uno', 'one'));
+
+// TIP: slice: nos extrae una seccion de una cadena y nos devuelve una nueva
+document.write('<p>' + 'slice: ' + frase.slice(7, 12));
+
+// TIP: small: // XXX:  nos reduce el tamaño de fuente de nuestro texto
+document.write('<p>' + 'small: ' + frase.small(7, 12));
+
+// TIP: split: nos divide la cadena aplicando separadores coma "," a cada elemento indicado
+//tb se puede indicar la cantidad de ocurrencias
+document.write('<p>' + 'split: ' + frase.split('o'));
+document.write('<p>' + 'split: ' + frase.split('o', 3));
+
+// TIP: startsWith: indica si una cadena comienza con un caracter o una cadena indicada
+// responde TRUE / FALSE
+document.write('<p>' + 'startsWith: ' + frase.startsWith('que'));
+
+// TIP: sub // XXX: OBSOLETO: muestra como subcadena
+document.write('<p>' + 'sub: ' + frase.sub(2));
+
+// TIP: substring: retorna la parte de la cadena entre los indices de inicio y fin
+document.write('<p>' + 'substring: ' + frase.substring(1, 5));
+
+// TIP: toLocaleLowerCase: nos convierte a misculas en el entorno local
+document.write('<p>' + 'toLocaleLowerCase: ' + frase.toLocaleLowerCase());
+
+// TIP: toLocaleUpperCase: nos convierte a mayusculas en el entorno local
+document.write('<p>' + 'toLocaleUpperCase: ' + frase.toLocaleUpperCase());
+
+// TIP: toLowerCase: nos convierte a minusculas
+document.write('<p>' + 'toLowerCase: ' + frase.toLowerCase());
+
+// TIP: toUpperCase: nos convierte a mayusculas
+document.write('<p>' + 'toUpperCase: ' + frase.toUpperCase());
+
+// TIP: trim: nos limpia los espacion no significativos de la cadena
+document.write('<p>' + 'trim: ' + frase2.trim());
